b79c6a8647b6c126daef3fc2d8a5cb38bf7a97f0
Aleks.lok <156440900+lokad337@users.noreply.github.com>
2024-03-16 12:50:53 +0300

    LoginPageTest стал наследником BaseTest. В BaseTest добавлен @Before

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,12 +1,16 @@
 package test;
 
+// Базовый класс который будет относится ко всем классам
+
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
-    @BeforeEach
-    void setUp() {
+
+    @Before
+    public void setUp() {
         open("https://xn--80afcdbalict6afooklqi5o.xn--p1ai/identity/account/login");
     }
 
--- src/test/java/test/LoginPageTest.java
+++ src/test/java/test/LoginPageTest.java
@@ -1,21 +1,21 @@
 package test;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import pages.LoginPage;
-
 import static com.codeborne.selenide.Selenide.open;
 
-public class LoginPageTest extends BaseTest {
 
+public class LoginPageTest extends BaseTest {
+    
 
     @Test
-    @DisplayName("Проверка входа с верными данными")
+    @DisplayName("Проверка страницы логин")
     public void TestLoginPositive(){
-        open("https://xn--80afcdbalict6afooklqi5o.xn--p1ai/identity/account/login");
-        //создание экземпляра класса LoginPage, теперь все методы оттуда будут доступны
-        new LoginPage().inputValidUserName();
 
+        // создание экземпляра класса LoginPage, теперь все методы оттуда будут доступны
+        new LoginPage().inputValidUserName();
     }
+
+
 }
